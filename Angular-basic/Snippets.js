6eba213a6fd5117ed90e07232c31e96973f99484
楼满花香 <ccto@outlook.com>
2016-02-22 00:20:19 +0800

    增加示例文件

---
+++ Angular-basic/Snippets.js
@@ -0,0 +1,19 @@
+function foo(name) {
+    console.log(name)
+}
+
+if (true) {}
+
+/**
+expressBlank
+*/
+function Person(name) {
+    this.name = name;
+}
+
+
+Person.prototype.speak = function(content) {
+    console.log(thi.name + "speak" + content)
+};
+
+
